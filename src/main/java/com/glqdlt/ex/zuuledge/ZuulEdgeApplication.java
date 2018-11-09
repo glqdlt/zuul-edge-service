120f02ba07f5ee84b843e4f7838fa1f58e8c903e
jhun <jhun@nm-4ones.com>
2018-11-09 10:00:06 +0900

    init

---
+++ src/main/java/com/glqdlt/ex/zuuledge/ZuulEdgeApplication.java
@@ -0,0 +1,12 @@
+package com.glqdlt.ex.zuuledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulEdgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulEdgeApplication.class, args);
+    }
+}
