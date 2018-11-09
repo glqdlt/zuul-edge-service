bc7a07c3b86c733358cb5b69a97395c85833691b
jhun <jhun@nm-4ones.com>
2018-11-09 11:59:57 +0900

    source

--- src/main/java/com/glqdlt/ex/zuuledge/ZuulEdgeApplication.java
+++ src/main/java/com/glqdlt/ex/zuuledge/ZuulEdgeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
-//https://cloud.spring.io/spring-cloud-netflix/multi/multi__router_and_filter_zuul.html 8.10번
 @EnableZuulProxy
-//@EnableZuulProxy 가 @EnableZuulServer 를 포함한다고 함.
 @SpringBootApplication
 public class ZuulEdgeApplication {
 
